e237d3813fccf427e0e936924ab5b235fd4d811f
Cheulong Sear <searcheulong@gmail.com>
2025-05-25 20:29:17 +0700

    install cors library

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from 'express'
-
+import cors from 'cors'
+import bodyParser from 'body-parser'
 const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
 const PORT = process.env.PORT || 3000
+
 
 app.get('/', (req, res) => {
   res.send('Test!')
