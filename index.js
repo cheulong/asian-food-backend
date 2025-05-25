7ceb100a8ff6c17d6a71839417a9bdfdd6748c21
Cheulong Sear <searcheulong@gmail.com>
2025-05-25 16:47:20 +0700

    dockerize the backend of asian food  webapp

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Test!')
+})
+
+app.get('/v1/menus', (req, res) => {
+    const menus = [
+        { id: 1, name: 'Sushi',price: 10.99 },
+        { id: 2, name: 'Ramen',price: 12.99 },
+        { id: 3, name: 'Tempura',price: 8.99 },
+        { id: 4, name: 'Sukiyaki',price: 9.99 },
+        { id: 5, name: 'Udon',price: 11.99 },
+        { id: 6, name: 'Bento',price: 13.99 },
+        { id: 7, name: 'Sashimi',price: 14.99 },
+        { id: 8, name: 'Japanese Curry',price: 15.99 },
+        { id: 9, name: 'Gyoza',price: 16.99 },
+        { id: 10, name: 'Miso Soup',price: 3.99 },
+    ]
+  res.status(200).json(menus)
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`)
+})
